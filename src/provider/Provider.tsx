4b74747b1ebbb8250265057587722583d095c5a6
odilovaslanbek20 <aslanbekodilov524@gmail.com>
2025-05-11 16:05:21 +0500

    ""

--- src/provider/Provider.tsx
+++ src/provider/Provider.tsx
@@ -1,12 +1,16 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import type { PropsWithChildren } from 'react'
+import type React from 'react'
 import { BrowserRouter as Router } from 'react-router-dom'
 
 const queryClient = new QueryClient()
 
-export default function Provider({ children }) {
+const Provider: React.FC = ({ children }: PropsWithChildren) => {
 	return (
-		<Router>
+		<Router> 
 			<QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 		</Router>
 	)
 }
+
+export default Provider
